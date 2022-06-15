8424e3e1a075db9cc64049ed1f6657ca3f3214ad
Muhammad Ansary <84735386+MuhammadAA@users.noreply.github.com>
2022-06-14 17:45:15 -0700

    Made game playable for 5 rounds
    Created a function called "game" which runs the function "playRound" 5 times using a for-loop. "game" function also stores the amount of times the player/ computer wins to display whether the computer or player won overall across 5 games.
    
    Added comments in the code which accurately describes the role of each function or subsection of code.
    
    Switched from "console.log()" to "alert()" statements to make the game more playable.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-function computerPlay () {
+function computerPlay () {      // Function to decide what the computer picks 
     let val = Math.floor(Math.random() * (3 - 1 +1) + 1); 
     
     let output;
@@ -15,44 +15,94 @@ function computerPlay () {
     return output;
 }
 
-//console.log(computerPlay())
-//console.log()
 
-function playRound (playerSelection, computerSelection) {
+// Plays 1 round of the game with the computer (required user to input their selection before hand, as well as the result from the computerPlay function)
+function playRound (playerSelection, computerSelection) {    
 
+    // Ensures casing doesnt become an issue 
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
+    playerWins = false;
+
     let outcome;
-    console.log(playerSelection)
-    console.log(computerSelection)
+        
     // Draw Situation
     if (playerSelection == computerSelection) {
         outcome = "Draw!";
     }
     
-    // Computer Wins Situation
+    // Computer Wins Situations
     else if (playerSelection == "rock" && computerSelection == "paper"){
-        outcome = "Computer Wins!";
+        outcome = "Paper beats Rock. Computer Wins!";
     }
     else if (playerSelection == "paper" && computerSelection == "scissors"){
-        outcome = "Computer Wins!";
+        outcome = "Scissors beats Paper. Computer Wins!";
     }
     else if (playerSelection == "scissors" && computerSelection == "rock") {
-        outcome = "Computer Wins!";
+        outcome = "Rock beats Scissors. Computer Wins!";
     }
 
-    // Player Wins Situation
+    // Player Wins Situations
     else if (computerSelection == "rock" && playerSelection == "paper"){
-        outcome = "Player Wins!";
+        outcome = "Paper beats Rock. Player Wins!";
+        playerWins = true;
     }
     else if (computerSelection == "paper" && playerSelection == "scissors"){
-        outcome = "Player Wins!";
+        outcome = "Scissors beats Paper. Player Wins!";
+        playerWins = true;
     }
     else if (computerSelection == "scissors" && playerSelection == "rock"){
-        outcome = "Player Wins!";
+        outcome = "Rock beats Scissors. Player Wins!";
+        playerWins = true;
     }
 
-    return outcome ;
+    // Returns multiple values as an array so that its easier to receive the two return values
+    return overall = [outcome, playerWins];
 }
 
-console.log(playRound("rock",computerPlay()))
+
+
+// Loops through the playRound function 5 times and keeps track of the number of times the computer or player wins and announces overall winner in the end
+function game() {
+    let computerWinCounter = 0;
+    let playerWinCounter = 0;
+
+    for (let i = 0; i < 5; i++){
+        playerSelection = prompt("Enter your pick: ");
+        computerSelection = computerPlay();
+        
+
+        // Splitting multiple returns from playRound function
+        overall = playRound(playerSelection,computerSelection);
+        outcome = overall[0];
+        playerWins = overall[1] 
+
+        // Tells player who won the round
+        alert(outcome);
+
+        // Updates count for the winner of the round
+        if (playerWins == false && outcome != "Draw!"){
+            computerWinCounter += 1;
+        }
+        else if (playerWins == true) {
+            playerWinCounter += 1;
+        }
+    }
+
+    // Alerts in browser the winner from the total of 5 matches
+    console.log("computer: " + computerWinCounter)
+    console.log("player: " + playerWinCounter)
+    if (computerWinCounter == playerWinCounter) {
+        alert("Draw!")
+    }
+    else if (computerWinCounter > playerWinCounter) {
+        alert("Computer Wins!")
+    }
+    else {
+        alert("Player Wins!")
+    }
+}
+
+
+// Running the game
+game();
